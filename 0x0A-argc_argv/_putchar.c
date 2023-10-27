3411bbac9ef5c1de9b825beb59690318f03b39c0
AbelAsiedu <--global>
2023-10-27 16:20:54 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/_putchar.c
+++ 0x0A-argc_argv/_putchar.c
@@ -24,34 +24,3 @@ int _putchar(char c)
 return (write(1, &c, 1));
                 
 }
-=============================
-0-whatsmyname.c code
-#include <stdio.h>
-                
-#include "main.h"
-                
-                
-/**
-                
- * main - prints the name of the program
-                
- * @argc: number of arguments
-                
- * @argv: array of arguments
-                
- *
-                
- * Return: Always 0 (Success)
-                
- */
-                
-int main(int argc __attribute__((unused)), char *argv[])
-                
-{
-                
-        printf("%s\n", *argv);
-                
-                
-        return (0);
-                
-}
