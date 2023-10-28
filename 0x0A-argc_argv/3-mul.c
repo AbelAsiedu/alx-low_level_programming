6b4d88068e93fc71d14b61f4c39d0d35d4ee23ee
AbelAsiedu <--global>
2023-10-27 17:33:55 -0700

    Correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-int mul(int a, int b) {
-    return a * b;
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result);
 }
 
-int main(int argc, char *argv[]) {
-    int num1, num2, result;
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
-    if (argc != 3) {
-        printf("Error\n");
-        return 1;
-    }
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
 
-    num1 = atoi(argv[1]);  // Use the standard atoi function here
-    num2 = atoi(argv[2]);  // Use the standard atoi function here
-    result = mul(num1, num2);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    printf("%d\n", result);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = mul(num1, num2);
 
-    return 0;
+	printf("%d\n", result);
+
+	return (0);
 }
