400fe1d0a854adc3a0732aab5342ba31e047b70f
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-01-20 01:53:43 +0000

    Update 100-atoi.c

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,27 +1,20 @@
-#include "main.h"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
- */
-char *_strncat(char *dest, char *src, int n)
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
+int _atoi(char *s)
 {
-	int space1 = 0, space2 = 0;
-
-	while (*(dest + space1) != '\0')
-	{
-		space1++;
-	}
-
-	while (space2 < n)
-	{
-		*(dest + space1) = *(src + space2);
-		if (*(src + space2) == '\0')
-			break;
-		space1++;
-		space2++;
-	}
-	return (dest);
+int i = 1;
+unsigned int num = 0;
+do {
+if (*s == '-')
+i *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *i);
 }
